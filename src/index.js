f16e5fff770e80c88feebbb3068afa7bdb702cf8
2kevink <kevink2@yandex.ru>
2018-09-14 03:31:09 +0300

    'not_enough'

--- src/index.js
+++ src/index.js
@@ -1,26 +1,86 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.tmp = [initialValue];
+    this.operands = ['^','*','/','-','+'];
   }
 
   add(number) {
-    // your implementation
+    this.tmp.push('+');
+    this.tmp.push(number);
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.tmp.push('-');
+    this.tmp.push(number);
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.tmp.push('*');
+    this.tmp.push(number);
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.tmp.push('/');
+    this.tmp.push(number);
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.tmp.push('^');
+    this.tmp.push(number);
+    return this;
+  }
+  
+  toString(){
+    for(var i=0; i<this.operands.length; i++){
+      var current = this.operands[i];
+      for(var j=0; j<this.tmp.length; j++){
+    
+        if(this.tmp[j]==current){
+        if(this.tmp[j]=='^') {
+          this.tmp[j] = Math.pow(this.tmp[j-1],this.tmp[j+1]);
+          this.tmp.splice(j + 1, 1);
+          this.tmp.splice(j - 1, 1);
+          j = 0;
+        }
+        if(this.tmp[j]=='*'){
+          this.tmp[j] = this.tmp[j+1]*this.tmp[j-1];
+          this.tmp.splice(j + 1, 1);
+          this.tmp.splice(j - 1, 1);
+          j = 0;
+        }
+        if(this.tmp[j]=='/'){
+          this.tmp[j] = this.tmp[j-1]/this.tmp[j+1];
+          this.tmp.splice(j + 1, 1);
+          this.tmp.splice(j - 1, 1);
+          j = 0;
+        }
+        
+        if(this.tmp[j]=='-'){
+          this.tmp[j] = this.tmp[j-1]-this.tmp[j+1];
+          this.tmp.splice(j + 1, 1);
+          this.tmp.splice(j - 1, 1);
+          j = 0;
+        }
+        if(this.tmp[j]=='+'){
+          this.tmp[j] = this.tmp[j+1]+this.tmp[j-1];
+          this.tmp.splice(j + 1, 1);
+          this.tmp.splice(j - 1, 1);
+          j = 0;
+        }
+        else{
+          continue;
+        }
+        console.log(this.tmp);
+        j=0;
+        }
+      }
+    }
+    //console.log(this.tmp);
+    return this.tmp[0];
   }
 }
 
